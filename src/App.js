9fc25f6c41a106098db717b74a8ad04bbf403005
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-19 04:42:43 -0700

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 /** @format */
 
 import "./App.css";
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-import StorefrontIcon from "@material-ui/icons/Storefront";
-import SearchIcon from "@material-ui/icons/Search";
+
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      <div className="header__logo"></div>
-      <StorefrontIcon className="header__logoImage" fontSize="large" />
-      <h2 className="header__logoTitle">eShop</h2>
-      <div className="header__search">
-        <input type="text" className="header__searchInput" />
-        <SearchIcon className="header__searchIcon" />
-      </div>
-
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
-        </div>
-        <div className="nav__item">
-          <ShoppingBasketIcon />
-          <span className="nav__itemLineTwo">0</span>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
