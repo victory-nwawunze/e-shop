852d847b77b1c7a23f6cb11967200b6adc9b4819
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-20 07:15:15 -0700

    creating Home

--- src/Product.js
+++ src/Product.js
@@ -5,20 +5,21 @@ import "./Product.css";
 
 function Product() {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>Title</p>
-          <p className="product__price">$30</p>
-          <div className="product__rating">❤❤</div>
-        </div>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
-          alt=""
-          srcset=""
-        />
-        <button>Add to Basket</button>
+    <div className="product">
+      <div className="product__info">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+          adipisicing elit. Fugiat, (provident).
+        </p>
+        <p className="product__price">$30</p>
+        <div className="product__rating">❤❤</div>
       </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+        alt=""
+        srcset=""
+      />
+      <button>Add to Basket</button>
     </div>
   );
 }
