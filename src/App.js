65212120b2250df4d961c52e612c369a16877eb6
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-20 06:56:08 -0700

    creating Home

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 import Header from "./Header";
 import Home from "./Home";
+import Product from "./Product";
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Product />
     </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div>
+      <div className="product">
+        <div className="product__info">
+          <p>Title</p>
+          <p className="product__price">$30</p>
+          <div className="product__rating">❤❤</div>
+        </div>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+          alt=""
+          srcset=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Product;
