4056f73c34f2b7ba6e507ddefee305f5023011e9
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-21 09:40:23 -0700

    creating Home

---
+++ src/Checkout.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+import "./Checkout.css";
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  );
+}
+
+export default Checkout;
